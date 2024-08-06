f379641a77ae147e60dfbea23445b5a3f5b4ce69
Jacques-lang <156355612+Jacques-lang@users.noreply.github.com>
2024-08-05 20:41:38 -0600

    Create difficultylist.py
    This is where the actual app takes its words from, ranging from easy words to hard ones

---
+++ difficultylist.py
@@ -0,0 +1,29 @@
+easiest_words = [
+    "all", "auto", "cat", "chat", "fox", "ice", "igloo", "joke", "join", "kind",
+    "koala", "late", "lemon", "lily", "moon", "name", "nose", "oak", "pack",
+    "run", "ship", "small", "taxi", "tidy", "tooth", "under", "vase", "view",
+    "well", "world", "x-ray", "you", "year", "zebra", "zeal"
+]
+
+medium_words = [
+    "arise", "bench", "best", "book", "camera", "candle", "class", "coffee",
+    "color", "dance", "dusk", "eerie", "finish", "frame", "free", "guitar",
+    "honey", "icon", "journal", "jump", "kangaroo", "kitchen", "laugh", "maze",
+    "melody", "minute", "moon", "night", "order", "pizza", "puppy", "puzzle",
+    "secret", "skill", "street", "style", "timer", "tourist", "travel", "truck",
+    "uplift", "uniform", "violin", "vision", "wander", "winter", "yoga", "yoyo",
+    "zigzag", "zone"
+]
+
+hardest_words = [
+    "anecdote", "alphabet", "awesome", "balloon", "basket", "birthday",
+    "briefcase", "cauliflower", "children", "classic", "classroom", "colorful",
+    "creative", "dinosaur", "doorknob", "elephant", "emerald", "electric",
+    "flowers", "frequent", "grocery", "haircut", "homemade", "hundred", "invest",
+    "introduce", "jovial", "kaleidoscope", "landscape", "learning", "magazine",
+    "marshmallow", "meditate", "monument", "motorcycle", "mountain", "negotiate",
+    "opposite", "octopus", "painting", "picnic", "pillow", "podcast",
+    "presentation", "restaurant", "revolve", "spaceship", "stargazing",
+    "sunrise", "together", "unicorn", "unique", "visitor", "volume",
+    "whirlwind", "xylophone", "yogurt", "yummy", "zoology"
+]
